a8f96356150b7ee9da3bc3f56b993fb30394d45b
ViliamJ <viliam180898@gmail.com>
2020-05-17 03:02:30 +0200

    Done

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import copy
 import re
 
-memory_in = open("memory_in.txt", "r")
-
-copy_from_in_to_out_do_pice = memory_in.read()
-
-memory_out_txt = open("memory_out.txt", "w")
-
-rules = open("rules.txt", "r")
-
-print(copy_from_in_to_out_do_pice, file=memory_out_txt)
-
-memory_out_txt.close()
-memory_in.close()
-
 
 class Rule:
     def __init__(self, index):
@@ -120,7 +107,7 @@ def get_number_of_variables(rule):
     return len(list_of_var)
 
 
-def execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs):
+def step_2_execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs):
     actual_condition = rule.conditions[depth]
 
     actual_condition_with_names = actual_condition
@@ -147,7 +134,7 @@ def execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs
         two_variables = actual_condition.replace("<>", "").replace("?", "").strip().split(" ")
         if dict_of_variables[two_variables[0]] != dict_of_variables[two_variables[1]]:
             if rule.max_depth != (depth + 1):
-                execute_condition(rule, dict_of_variables, depth + 1, memory_out, helper_outputs)
+                step_2_execute_condition(rule, dict_of_variables, depth + 1, memory_out, helper_outputs)
             else:
                 number_of_variables = get_number_of_variables(rule)
 
@@ -157,13 +144,10 @@ def execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs
                     for key, value in dict_of_variables.items():
                         new_helper_output = new_helper_output.replace(f"?{key}", value)
 
-                    new_helper_output = f"{rule.name}, {new_helper_output}"
+                    new_helper_output = f"{rule.name},{new_helper_output}"
 
                     if new_helper_output not in helper_outputs:
                         helper_outputs.append(new_helper_output)
-
-
-        print(f"{rule.name}")
     else:
         for output in useful_facts:
             new_variable_names = output.replace(actual_condition_with_names, "").replace("  ", " ").strip().split(" ")
@@ -173,7 +157,7 @@ def execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs
                 new_dict_variables[new_variable_keys[index]] = new_variable_names[index]
 
             if rule.max_depth != (depth + 1):
-                execute_condition(rule, new_dict_variables, depth + 1, memory_out, helper_outputs)
+                step_2_execute_condition(rule, new_dict_variables, depth + 1, memory_out, helper_outputs)
             else:
                 number_of_variables = get_number_of_variables(rule)
 
@@ -182,19 +166,86 @@ def execute_condition(rule, dict_of_variables, depth, memory_out, helper_outputs
                     for key, value in new_dict_variables.items():
                         new_helper_output = new_helper_output.replace(f"?{key}", value)
 
-                    new_helper_output = f"{rule.name}, {new_helper_output}"
+                    new_helper_output = f"{rule.name},{new_helper_output}"
 
                     if new_helper_output not in helper_outputs:
                         helper_outputs.append(new_helper_output)
 
 
+def step_3_execute_filter(helper_outputs, memory):
+    for x in range(len(helper_outputs) - 1, -1, -1):
+        item = helper_outputs[x]
+        item_list = item.split(",")
+        correct_output = []
+
+        for y in range(1, len(item_list)):
+            actual_item_action_string = item_list[y].strip().split(" ")[0]
+            actual_item_string = item_list[y].replace(f"{actual_item_action_string} ", "")
+
+            if actual_item_action_string == "pridaj" and actual_item_string not in memory.memory:
+                correct_output.append(item_list[y])
+            elif actual_item_action_string == "vymaz" and actual_item_string in memory.memory:
+                correct_output.append(item_list[y])
+            elif actual_item_action_string == "sprava":
+                correct_output.append(item_list[y])
+
+        flag_is_correct = False
+        for item_output in correct_output:
+            item_output_action = item_output.split(" ")[0]
+            if item_output_action != "sprava":
+                flag_is_correct = True
+                break
+
+        if flag_is_correct:
+            correct_output_string = ""
+            for correct_output_item in correct_output:
+                correct_output_string = f"{correct_output_string},{correct_output_item}"
+            helper_outputs[x] = f"{item_list[0]}{correct_output_string}"
+
+        else:
+            helper_outputs.pop(x)
+
+
+def step_5_execute_first(helper_outputs, memory):
+    first_rules = helper_outputs[0].split(",")[1:]
+
+    for rule in first_rules:
+        action = rule.split(" ")[0]
+        new_rule = rule.replace(f"{action} ", "")
+
+        if action == "pridaj":
+            memory.memory.append(new_rule)
+        elif action == "vymaz":
+            memory.memory.remove(new_rule)
+        elif action == "sprava":
+            print(new_rule)
+        else:
+            print("WRONG MADAFAKA")
+
+    helper_outputs.pop(0)
+
+
 if __name__ == '__main__':
+
+    memory_in = open("memory_in.txt", "r")
+
+    copy_from_in_to_out_do_pice = memory_in.read()
+
+    memory_out_txt = open("memory_out.txt", "w")
+
+    rules = open("rules.txt", "r")
+
+    print(copy_from_in_to_out_do_pice, file=memory_out_txt)
+
+    memory_out_txt.close()
+    memory_in.close()
+
     helper_outputs = []
-    prd = Rule(1)
+
     rules.seek(0)
     number_of_rules = rules.readlines()
 
-    fuj = Memory_output()
+    memory_out_list = Memory_output()
 
     object_rules = []
 
@@ -203,6 +254,22 @@ if __name__ == '__main__':
 
         object_rules.append(object)
 
-    for i in range(len(object_rules)):
-        execute_condition(object_rules[i], {}, 0, fuj, helper_outputs)
-    print(helper_outputs)
+    all_done_flag = False
+
+    # while all_done_flag is False:
+    for _ in range(20):
+
+        for i in range(len(object_rules)):
+            step_2_execute_condition(object_rules[i], {}, 0, memory_out_list, helper_outputs)
+
+        step_3_execute_filter(helper_outputs, memory_out_list)
+
+        if helper_outputs:
+            step_5_execute_first(helper_outputs, memory_out_list)
+        else:
+            all_done_flag = True
+
+    final_out = open("memory_out.txt", "w")
+    [print(x, file=final_out) for x in memory_out_list.memory]
+    print(memory_out_list.memory)
+    final_out.close()
--- test.py
+++ test.py
@@ -9,4 +9,7 @@ for i in range(len(str)):
         if str[i + 1] not in list_of_var:
             list_of_var.append(str[i + 1])
 
+slovo = "clovek"
+var = [character.replace("c"," ") for character in slovo]
+print(var)
 print(list_of_var)
